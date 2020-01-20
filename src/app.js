ee55b062cd01f94a1c529c27abc26c3d07b0283a
nalbam <me@nalbam.com>
2020-01-20 15:30:25 +0900

    SubscriptionResult

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ import API, {
   graphqlOperation
 } from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
+
 import {
   createTodo
 } from './graphql/mutations'
+import {
+  listTodos
+} from './graphql/queries'
+import {
+  onCreateTodo
+} from './graphql/subscriptions'
 
 import awsconfig from './aws-exports';
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
+
+const QueryResult = document.getElementById('QueryResult');
+const SubscriptionResult = document.getElementById('SubscriptionResult');
 
 async function createNewTodo() {
   const todo = {
@@ -20,6 +30,15 @@ async function createNewTodo() {
   }))
 }
 
+async function getData() {
+  QueryResult.innerHTML = `QUERY RESULTS`;
+  API.graphql(graphqlOperation(listTodos)).then((evt) => {
+    evt.data.listTodos.items.map((todo, i) =>
+      QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`
+    );
+  })
+}
+
 const MutationButton = document.getElementById('MutationEventButton');
 const MutationResult = document.getElementById('MutationResult');
 
@@ -29,3 +48,13 @@ MutationButton.addEventListener('click', (evt) => {
     MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`
   })
 });
+
+API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+  next: (evt) => {
+    SubscriptionResult.innerHTML = `SUBSCRIPTION RESULTS`
+    const todo = evt.value.data.onCreateTodo;
+    SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`
+  }
+});
+
+getData();

8ec4acfa66521fdefc133fe0201eda7c87a2430f
nalbam <me@nalbam.com>
2020-01-30 14:05:15 +0000

    Updates

---
+++ precache-manifest.c09ebabe7d1f6a1ce63e0376eb8be6ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be54be7ee8f72533791dafe8a55dcbcf",
+    "url": "/sample-amplify/index.html"
+  },
+  {
+    "revision": "48bc6ccaf75009c0462f",
+    "url": "/sample-amplify/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "86f277dc114d2c67e14f",
+    "url": "/sample-amplify/static/css/main.4a0acab0.chunk.css"
+  },
+  {
+    "revision": "48bc6ccaf75009c0462f",
+    "url": "/sample-amplify/static/js/2.2b5b42ad.chunk.js"
+  },
+  {
+    "revision": "45330654ab392943d08691fe1abbcc35",
+    "url": "/sample-amplify/static/js/2.2b5b42ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86f277dc114d2c67e14f",
+    "url": "/sample-amplify/static/js/main.4ef0b598.chunk.js"
+  },
+  {
+    "revision": "45197a0c63ab73a730a7",
+    "url": "/sample-amplify/static/js/runtime-main.c8a33233.js"
+  }
+]);
